f517ada5f92dde33020ba71f71ec8dd0fa402d70
Thalysson <thalyssonsg.dev@gmail.com>
2022-11-22 10:21:30 -0300

    g

--- src/app/exer02/exer02.component.ts
+++ src/app/exer02/exer02.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from './exer02.interface';
 
 @Component({
   selector: 'app-exer02',
@@ -6,27 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exer02.component.css']
 })
 export class Exer02Component implements OnInit {
-  itens: string[] = [];
-  tarefa: string = '';
+
+  itens: Item[] = [];
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  inserir() {
-    this.itens.push(this.tarefa)
+
+  inserir(description: string) {
+    if (description.trim() != '') {
+      this.itens.push({
+        description: description.trim(),
+        done: false
+      });
+    }
   }
 
   remover(index: number) {
     this.itens.splice(index, 1)
-    alert(this.itens)
   }
 
-  changeItem(i: number) {
-    // if (i && i != null) {
-    //   document.getElementById(`${i}`).style.color = "green";
-    // }
+  change(index: number) {
+    this.itens[index].done = !this.itens[index].done;
   }
 
 }
